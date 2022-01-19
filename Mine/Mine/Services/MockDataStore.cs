555265be979fc22ab6d4b2b5fc356eec4b329f95
4133292 <adornipadu@seattleu.edu>
2022-01-18 20:20:01 -0800

    Updated Default data in MockDataStore.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -14,12 +14,11 @@ namespace Mine.Services
         {
             items = new List<ItemModel>()
             {
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Smile", Description="Causes to laugh.", Value=7 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Cry", Description="Causes to cry.", Value=3 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Hate", Description="Causes to hate.", Value=1 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Angry", Description="Causes to get angry.", Value=5},
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Love", Description="Causes to love.", Value=9 },
             };
         }
 
