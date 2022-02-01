ad2cdb50e52d53cab767df5e876d85f816ad98d8
4133292 <adornipadu@seattleu.edu>
2022-02-01 12:47:40 -0800

    Added CreateAsync to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -37,9 +37,24 @@ namespace Mine.Services
             }
         }
 
-        public Task<bool> CreatAsync(ItemModel item)
+        /// <summary>
+        /// Insertion of Item
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public async Task<bool> CreatAsync(ItemModel item)
         {
-            throw new NotImplementedException();
+            
+            if(item == null)
+            {
+                return false;
+            }
+            var result = await Database.InsertAsync(item);
+            if (result == 0)
+            {
+                return false;
+            }
+            return true;
         }
 
         public Task<bool> UpdateAsync(ItemModel item)
