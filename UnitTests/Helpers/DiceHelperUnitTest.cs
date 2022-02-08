024eb2b9faf308792e9a2db09eb25ec8adba6530
4133292 <adornipadu@seattleu.edu>
2022-02-08 00:03:10 -0800

    Modified the DiceHelperUnitTest.cs by adding the test.

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -8,7 +8,22 @@ namespace UnitTests.Helpers
     public class DiceHelperTests
     {
         [Test]
-        public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_AND_6()
+        public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 1);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+
+        public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
         {
             // Arrange
 
@@ -19,7 +34,7 @@ namespace UnitTests.Helpers
 
             // Assert 
             Assert.AreEqual(true, result >= 1);
-            Assert.AreEqual(true, result <= 6);
+            Assert.AreEqual(true, result >= 6);
         }
     }
 }
