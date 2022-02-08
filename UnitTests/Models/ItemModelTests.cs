5e2ba2ba46f47e580e985485626fa61df06eb529
4133292 <adornipadu@seattleu.edu>
2022-02-08 00:17:42 -0800

    Modified the ItemModelTests.cs with correct tests.

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -12,15 +12,51 @@ namespace UnitTests.Models
         [Test]
         public void ItemModel_Constructor_Valid_Default_Should_Pass()
         {
-            // Arrange
+            //Arrage
 
-            // Act
+            //Act
             var result = new ItemModel();
 
-            // Reset
+            //Reset
 
-            // Assert 
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void ItemModel_Set_Get_Valid_Default_Should_Pass()
+        {
+            //Arrage
+
+            //Act
+            var result = new ItemModel();
+            result.Description = "Description";
+            result.Id = "ID";
+            result.Text = "Text";
+            result.Value = 1;
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual("Description", result.Description);
+            Assert.AreEqual("ID", result.Id);
+            Assert.AreEqual("Text", result.Text);
+            Assert.AreEqual(1, result.Value);
+        }
+
+        [Test]
+        public void ItemModel_Get_Valid_Default_Should_Pass()
+        {
+            //Arrage
+
+            //Act
+            var result = new ItemModel();
+
+            //Reset
+
+            //Assert
             Assert.AreEqual(0, result.Value);
+
         }
     }
 }
